b34f87052211efc3bc9e117196c15db0831b7995
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2021-09-06 14:42:22 +0200

    Commit 8

---
+++ Assets/GhostManagerScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public struct GhostTranform
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    
+    public GhostTranform(Transform transform)
+    {
+        position = transform.position;
+        rotation = transform.rotation;
+    }
+    
+}
+public class GhostManagerScript : MonoBehaviour
+{
+    public Transform kart;
+    public Transform ghostKart;
+    
+    public bool recording;
+    public bool playing;
+
+    private List<GhostTranform> listGhostTranformer = new List<GhostTranform>();
+    private GhostTranform lastGhostTranformer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (recording)
+        {
+            if (kart.position != lastGhostTranformer.position || kart.rotation != lastGhostTranformer.rotation)
+            {
+                GhostTranform ghostTranform = new GhostTranform(kart);
+                listGhostTranformer.Add(ghostTranform);
+                lastGhostTranformer = ghostTranform;
+            }
+        }
+
+        if (playing)
+        {
+            Play();
+        }
+    }
+
+    private void Play()
+    {
+        ghostKart.gameObject.SetActive(true);
+        StartCoroutine("PlayGame");
+        playing = false;
+    }
+
+    public IEnumerable PlayGame()
+    {
+        for (int count = 0; count < listGhostTranformer.Count; count++)
+        {
+            ghostKart.position = listGhostTranformer[count].position;
+            ghostKart.rotation = listGhostTranformer[count].rotation;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/moveOtherCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOtherCar : MonoBehaviour
+{
+    
+    public Transform kart;
+    public Transform otherKart;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        otherKart.position = new Vector3(kart.position.x - 3, kart.position.y,kart.position.z - 4);
+    }
+}
